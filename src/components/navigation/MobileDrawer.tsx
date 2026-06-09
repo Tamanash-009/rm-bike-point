3e574bb82e17e2b6835b8cf46a640039875db07c
Tamanash Chakraborty <Tamanash-009@users.noreply.github.com>
2026-06-10 02:08:28 +0530

    fix: remove lg:hidden from MobileDrawer to show on desktop

--- src/components/navigation/MobileDrawer.tsx
+++ src/components/navigation/MobileDrawer.tsx
@@ -58,7 +58,7 @@ export default function MobileDrawer({ isOpen, onClose, cartCount }: MobileDrawe
   return (
     <AnimatePresence>
       {isOpen && (
-        <div className="fixed inset-0 z-[120] lg:hidden">
+        <div className="fixed inset-0 z-[120]">
           {/* Backdrop */}
           <motion.div 
             initial={{ opacity: 0 }}
